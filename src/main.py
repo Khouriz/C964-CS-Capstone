bd21b8644fd2f4a0bafd82188c1d21e0c42c352d
Khouriz <andrewz2473@gmail.com>
2023-08-18 16:13:11 -0700

    Provides example inputs, New Regression Model, Main changes, changes to notebook
    Provides the users an example of an input *.csv file.  Remade model using scaled times and amount increasing accuracy. This also results in a modification to the jupyter notebook to provide the code block for scaling and replacing source values. Added more functionality to main.py.

--- src/main.py
+++ src/main.py
@@ -25,26 +25,48 @@ import pandas as pd
 
 LRMODEL = joblib.load("./src/LogisticRegressionML/LogisticRegressionModel.joblib")
 
+filepath = None
+df = None
+
 def BrowseForFile():
     pass
 
-def LogisticRegressionPrediction():
+def DataPreprocessing():
     pass
+
+def LogisticRegressionPrediction():
+    if df is None:
+        header = "No Imported Data"
+        body = "You need to import data ot the application using Browse."
+        PopUpHandler(header,body)
+
+def PopUpHandler(header:str, body:str):
+    popup = Toplevel()
+    popup.title(header)
+
+    label = Label(popup, text=body)
+    label.pack(padx=20, pady=20)
+
+    close_button = Button(popup, text="Close", command=popup.destroy)
+    close_button.pack()
+    popup.wait_window(popup)
 
 #Create Tk window as set attributes
 window = Tk()
 window.title("Crazy Corp Credit Fraud Detection")
 window.geometry("500x300")
+window.resizable(height=False, width=False)
 window.config(background="white")
 
 #Set base buttons
 exit_button = Button(window, text="Exit", background="red", command=exit)
 browse_button = Button(window,text="Browse", command=BrowseForFile)
+submit_button = Button(window, text="Submit", command=LogisticRegressionPrediction)
 
 #Button locations
 exit_button.place(x=460, y=260)
 browse_button.place(x=400,y=260)
-
+submit_button.place(x=340, y=260)
 
 window.mainloop()
 
